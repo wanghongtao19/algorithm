0ee47844035608f849bc972f45b229e529cb258b
wanghongtao <wanghongtao@yidian-inc.com>
2020-06-11 11:26:21 +0800

    add

---
+++ src/main/java/com/wht/binarysearch/Solution.java
@@ -0,0 +1,14 @@
+package com.wht.binarysearch;
+
+/**
+ * Created by wanghongtao on 2020-06-11.
+ */
+public class Solution {
+
+
+    public static void main(String[] args) {
+        System.out.println("this is maven project");
+    }
+
+
+}
